96ae0566b98aa780da3e24132e4f49b5f9aec4e6
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-05-19 10:26:24 +0900

    docker 자동 빌드 및 aws 연결

--- src/test/java/com/sprint/mission/filemanagementservice/FilemanagementServiceApplicationTests.java
+++ src/test/java/com/sprint/mission/filemanagementservice/FilemanagementServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.sprint.mission.filemanagementservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FilemanagementServiceApplicationTests {
 
   @Test
